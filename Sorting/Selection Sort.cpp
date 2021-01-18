6f3e977a61b84143fb8646fefa6cead407427013
MuhammadKA <muhammad.abbass@outlook.com>
2021-01-18 16:53:06 +0200

    SelectionSort.cpp

--- Sorting/Selection Sort.cpp
+++ Sorting/Selection Sort.cpp
@@ -9,7 +9,11 @@ vector<int> selectionSort(vector<int> array) {
   // Write your code here.
   if (array.empty())
     return {};
-  for (int startIdx = 0; startIdx < array.size(); startIdx++) {
+  // startIdx is the index of the first number in the unsorted sub-list
+  // we just need to go to the last index - 1 in the array, because when our
+  // unsorted list contains only 1 number, then this number is in its final
+  // position
+  for (int startIdx = 0; startIdx < array.size() - 1; startIdx++) {
     int j = startIdx + 1;
     int smallest_index = startIdx;
     while (j < array.size()) {
