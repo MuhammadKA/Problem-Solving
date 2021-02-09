15a5766c04602268adcc7b9209410fbfbf517322
MuhammadKA <muhammad.abbass@outlook.com>
2021-02-09 19:18:10 +0200

    firstDuplicateValue.cpp

--- Arrays/firstDuplicateValue.cpp
+++ Arrays/firstDuplicateValue.cpp
@@ -18,3 +18,40 @@ int firstDuplicateValue(vector<int> array) {
     return -1;
   return array[minimumSecondIndex];
 }
+
+// -------------- < Time Optimal Solution | Worse Space > ------
+
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+// O(n) time | O(n) space
+int firstDuplicateValue(vector<int> array) {
+  unordered_map<int, bool> seenBefore;
+  for (int value : array) {
+    if (seenBefore.find(value) != seenBefore.end()) {
+      return value;
+    }
+    seenBefore[value] = true;
+  }
+  return -1;
+}
+
+// -------------- < Best Optimal Solution > ------
+
+
+#include <vector>
+using namespace std;
+
+// O(n) time | O(1) space
+int firstDuplicateValue(vector<int> array) { 
+	for (int value : array) {
+		int absValue = abs(value);
+		if (array[absValue - 1] < 0) {
+			return absValue;
+		}
+		array[absValue - 1] *= -1;
+	}
+	return -1; 
+}
+
