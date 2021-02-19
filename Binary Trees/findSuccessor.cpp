e0fa1f712610f042d6e6688b1eefff7bc00b89f5
MuhammadKA <muhammad.abbass@outlook.com>
2021-02-19 16:25:36 +0200

    Add findSuccessor.cpp

--- Binary Trees/findSuccessor.cpp
+++ Binary Trees/findSuccessor.cpp
@@ -34,3 +34,42 @@ void getInOrderTraversalOrder(BinaryTree *node, vector<BinaryTree *> &order) {
   order.push_back(node);
   getInOrderTraversalOrder(node->right, order);
 }
+
+// ---------------------- < Optimal Solution > ------------------
+
+using namespace std;
+
+// This is an input class. Do not edit.
+class BinaryTree {
+public:
+  int value;
+  BinaryTree *left = nullptr;
+  BinaryTree *right = nullptr;
+  BinaryTree *parent = nullptr;
+
+  BinaryTree(int value) { this->value = value; }
+};
+// O(d) time | O(1) space 
+BinaryTree *findLeftMostChild(BinaryTree *node) {
+  BinaryTree *currentNode = node;
+  while (currentNode->left != nullptr) {
+    currentNode = currentNode->left;
+  }
+  return currentNode;
+}
+
+BinaryTree *findRightMostParent(BinaryTree *node) {
+  BinaryTree *currentNode = node;
+  while (currentNode->parent != nullptr &&
+         currentNode->parent->right == currentNode) {
+    currentNode = currentNode->parent;
+  }
+  return currentNode->parent;
+}
+
+BinaryTree *findSuccessor(BinaryTree *tree, BinaryTree *node) {
+  // Write your code here.
+  if (node->right != nullptr)
+    return findLeftMostChild(node->right);
+  return findRightMostParent(node);
+}
