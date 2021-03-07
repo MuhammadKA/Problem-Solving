847528cfd488c3acca2323bfc37e619c4f8662c5
MuhammadKA <muhammad.abbass@outlook.com>
2021-03-07 13:11:19 +0200

    Add Hackerrank problem

--- Arrays/leftRotation.cpp
+++ Arrays/leftRotation.cpp
@@ -1,7 +1,35 @@
+/*
+A left rotation operation on an array shifts each of the array's elements 1 unit
+to the left. For example, if  left rotations are performed on array [1, 2, 3, 4, 5], then the
+array would become [3, 4, 5, 1, 2]. Note that the lowest index item moves to the highest index
+in a rotation. This is called a circular array.
+
+Given an array  of  integers and a number, , perform  left rotations on the
+array. Return the updated array to be printed as a single line of
+space-separated integers.
+
+Function Description
+
+Complete the function rotLeft in the editor below.
+
+rotLeft has the following parameter(s):
+
+int a[n]: the array to rotate
+int d: the number of rotations
+Returns
+
+int a'[n]: the rotated array
+Input Format
+
+The first line contains two space-separated integers  and , the size of  and the
+number of left rotations. The second line contains  space-separated integers,
+each an .
+
+
+*/
+
 #include <bits/stdc++.h>
-
 using namespace std;
-
 vector<string> split_string(string);
 
 // Complete the rotLeft function below.
