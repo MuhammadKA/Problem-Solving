071c20cab8b0ef9f58f24bdae7d81b273debedc9
MuhammadKA <muhammad.abbass@outlook.com>
2021-03-20 13:48:19 +0200

    Add threeNumberSum.cpp

--- Sorting/threeNumberSort.cpp
+++ Sorting/threeNumberSort.cpp
@@ -27,3 +27,30 @@ vector<int> threeNumberSort(vector<int> array, vector<int> order) {
 
   return array;
 }
+
+// -------------------------------------------- < Optimized Solution > -----------------------------------------
+
+// O(n) time | O(1) space - Most Optimal solution, requieres exactly one
+// traverse in the array
+vector<int> threeNumberSort(vector<int> array, vector<int> order) {
+  // Write your code here.
+  int firstValue = order[0];
+  int secondValue = order[1];
+
+  int firstIdx = 0;
+  int secondIdx = 0;
+  int thirdIdx = array.size() - 1;
+
+  while (secondIdx <= thirdIdx) {
+    int value = array[secondIdx];
+
+    if (value == firstValue) {
+      swap(array[secondIdx++], array[firstIdx++]);
+    } else if (value == secondValue) {
+      secondIdx++;
+    } else {
+      swap(array[secondIdx], array[thirdIdx--]);
+    }
+  }
+  return array;
+}
